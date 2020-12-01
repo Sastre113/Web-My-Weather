05d108bbb6538c48e40f31c254824ac4b90a24db
Miguel <sastre113@gmail.com>
2020-12-01 13:04:05 +0100

    añadida cosas

--- public/js/myWeather.js
+++ public/js/myWeather.js
@@ -1,46 +1,156 @@
-'use strict';
+"use strict";
 
 var appRoot = document.getElementById('elTiempo');
 var miTiempo = {};
+var miISO = {};
+var url = "";
 
 var render = function render(myWeather) {
     var template = React.createElement(
-        'div',
+        "div",
         null,
         React.createElement(
-            'p',
+            "p",
             null,
-            ' ',
+            " ",
             myWeather.data.name,
-            ' '
+            " "
         ),
         React.createElement(
-            'p',
+            "p",
             null,
-            ' ',
+            " ",
             myWeather.data.weather[0].description,
-            ' '
+            " "
         ),
         React.createElement(
-            'p',
+            "p",
             null,
-            '  ',
-            React.createElement('img', { src: 'http://openweathermap.org/img/wn/' + myWeather.data.weather[0].icon + '@2x.png\n                ' })
+            "  ",
+            React.createElement("img", { src: "http://openweathermap.org/img/wn/" + myWeather.data.weather[0].icon + "@2x.png\n                " })
+        ),
+        React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "form",
+                null,
+                React.createElement(
+                    "div",
+                    { id: "camposTiempo" },
+                    React.createElement(
+                        "label",
+                        { "for": "" },
+                        "Poblacion"
+                    ),
+                    React.createElement("input", { type: "text", id: "datoPoblacion" }),
+                    React.createElement(
+                        "label",
+                        { "for": "" },
+                        "Comunidad"
+                    ),
+                    React.createElement("input", { type: "text", id: "datoComunidad" })
+                ),
+                React.createElement(
+                    "label",
+                    { "for": "" },
+                    "Usando AlphaCODE"
+                ),
+                React.createElement("input", { type: "text", id: "datosISO" })
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+var render2 = function render2(iso3166) {
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            iso3166.data.name
+        ),
+        React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "form",
+                null,
+                React.createElement(
+                    "div",
+                    { id: "camposTiempo" },
+                    React.createElement(
+                        "label",
+                        { "for": "" },
+                        "Poblacion"
+                    ),
+                    React.createElement("input", { type: "text", id: "datoPoblacion" }),
+                    React.createElement(
+                        "label",
+                        { "for": "" },
+                        "Comunidad"
+                    ),
+                    React.createElement("input", { type: "text", id: "datoComunidad" })
+                ),
+                React.createElement(
+                    "label",
+                    { "for": "" },
+                    "Usando AlphaCODE"
+                ),
+                React.createElement("input", { type: "text", id: "datosISO" })
+            )
         )
     );
     ReactDOM.render(template, appRoot);
 };
 
 var peticion = document.querySelector('#botonTiempo');
+var peticion2 = document.querySelector('#botonAlpha');
+var datosIntroducidos = document.querySelector("#camposTiempo");
+
+datosIntroducidos.addEventListener('input', function (e) {
+    e.preventDefault();
+    console.log("Datos introducidos en el formulario");
+
+    console.log(e.target.value);
+    if (e.target.value !== "") document.querySelector("#datosISO").disabled = true;else document.querySelector("#datosISO").disabled = false;
+});
 
 peticion.addEventListener('click', function (e) {
     e.preventDefault();
     console.log("Boton pulsado");
-    // miTiempo = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Córdoba,Andalucía,ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d',);
 
-    miTiempo = axios.get('http://api.openweathermap.org/data/2.5/weather?q=Córdoba,Andalucía,ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d');
+    var poblacion = document.querySelector("#datoPoblacion").value;
+    var comunidad = document.querySelector("#datoComunidad").value;
+
+    // Estos no se usan
+    // let url = 'http://api.openweathermap.org/data/2.5/weather?q=Córdoba,Andalucía,ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d';
+
+    // let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + poblacion + ',' + comunidad + ',ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d';ç
+
+    url = "http://api.openweathermap.org/data/2.5/weather?q=" + poblacion + "," + comunidad + ",ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d";
+
+    miTiempo = axios.get(url);
 
     miTiempo.then(function (myWeather) {
         render(myWeather);
+    });
+});
+
+peticion2.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log("Boton pulsado");
+
+    var iso = document.querySelector("#datosISO").value;
+    url = "https://restcountries.eu/rest/v2/alpha/" + iso;
+
+    miISO = axios.get(url);
+
+    miISO.then(function (iso) {
+        render2(iso);
+    }).catch(function (error) {
+        console.error("ESTO ES UN ERROR");
     });
 });
--- src/app.js
+++ src/app.js
@@ -1,34 +1,106 @@
 const appRoot = document.getElementById('elTiempo');
 let miTiempo = {};
+let miISO = {};
+let url = "";
 
-let render = (myWeather) =>{
+let render = (myWeather) => {
     const template = (
-            <div>
-                <p> {myWeather.data.name} </p>
-                <p> {myWeather.data.weather[0].description} </p>  
-                <p>  <img src={`http://openweathermap.org/img/wn/${myWeather.data.weather[0].icon}@2x.png
+        <div>
+            <p> {myWeather.data.name} </p>
+            <p> {myWeather.data.weather[0].description} </p>
+            <p>  <img src={`http://openweathermap.org/img/wn/${myWeather.data.weather[0].icon}@2x.png
                 `}></img>
-                 </p>  
+            </p>
+            <div>
+                <form>
+                    <div id="camposTiempo">
+                        <label for="">Poblacion</label>
+                        <input type="text" id="datoPoblacion" />
+                        <label for="">Comunidad</label>
+                        <input type="text" id="datoComunidad" />
+                    </div>
+                    <label for="">Usando AlphaCODE</label>
+                    <input type="text" id="datosISO" />
+                </form>
             </div>
-            
-         
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+
+let render2 = (iso3166) => {
+    const template = (
+        <div>
+            <h1>{iso3166.data.name}</h1>
+            <div>
+                <form>
+                    <div id="camposTiempo">
+                        <label for="">Poblacion</label>
+                        <input type="text" id="datoPoblacion" />
+                        <label for="">Comunidad</label>
+                        <input type="text" id="datoComunidad" />
+                    </div>
+                    <label for="">Usando AlphaCODE</label>
+                    <input type="text" id="datosISO" />
+                </form>
+            </div>
+        </div>
     );
     ReactDOM.render(template, appRoot);
 };
 
 const peticion = document.querySelector('#botonTiempo');
+const peticion2 = document.querySelector('#botonAlpha');
+const datosIntroducidos = document.querySelector("#camposTiempo");
 
-peticion.addEventListener('click',  (e) => {
+datosIntroducidos.addEventListener('input', (e) => {
     e.preventDefault();
-    console.log("Boton pulsado")
-    // miTiempo = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Córdoba,Andalucía,ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d',);
+    console.log("Datos introducidos en el formulario");
 
-    miTiempo = axios.get('http://api.openweathermap.org/data/2.5/weather?q=Córdoba,Andalucía,ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d',);
+    console.log(e.target.value)
+    if (e.target.value !== "")
+        document.querySelector("#datosISO").disabled = true;
+    else
+        document.querySelector("#datosISO").disabled = false;
+});
 
-    miTiempo.then((myWeather) =>{
+
+peticion.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Boton pulsado");
+
+    let poblacion = document.querySelector("#datoPoblacion").value;
+    let comunidad = document.querySelector("#datoComunidad").value;
+
+    // Estos no se usan
+    // let url = 'http://api.openweathermap.org/data/2.5/weather?q=Córdoba,Andalucía,ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d';
+
+    // let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + poblacion + ',' + comunidad + ',ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d';ç
+
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${poblacion},${comunidad},ES&appid=58e4fe89a463d7b20eec7ef72de9ec4d`;
+
+
+    miTiempo = axios.get(url,);
+
+    miTiempo.then((myWeather) => {
         render(myWeather);
     });
+});
 
-    
+peticion2.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Boton pulsado");
+
+    let iso = document.querySelector("#datosISO").value;
+    url = `https://restcountries.eu/rest/v2/alpha/${iso}`;
+
+    miISO = axios.get(url,);
+
+    miISO.then((iso) => {
+        render2(iso);
+    }).catch((error) => {
+        console.error("ESTO ES UN ERROR");
+    });
 });
 
